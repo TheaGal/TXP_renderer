705122bbf1527b706fefb67145cdb06af42e2fce
TheaGal <timoguy99004@gmail.com>
2026-02-08 03:40:01 -0800

    WIP: small interface plan

--- include/txp_renderer/renderer.h
+++ include/txp_renderer/renderer.h
@@ -5,6 +5,8 @@
 #include <atomic>
 #include <cstdint>
 #include <string>
+#include <unordered_map>
+#include <vector>
 
 
 namespace TXP
@@ -19,9 +21,24 @@ public:
     Renderer(std::string const& title, int32_t width, int32_t height);
     ~Renderer();  // For pimpl.
 
-    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
+    // Asset loading.
+
+    /// Adds geometry material to renderer.
+    void add_material(std::string const& material_name,
+                      std::string const& shader_name,
+                      std::unordered_map<std::string, std::string> const& shader_params);
+
+    /// Adds set of geometry material to renderer index.
+    void add_material_set(std::string const& mat_set_name, std::vector<std::string>&& materials);
+
+    /// Adds model to renderer.
+    void add_model(std::string const& model_name,
+                   std::string const& file_ext,
+                   std::string const& default_mat_set_name);
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     // Render loop.
-    //
 
     /// Runs render loop until `shutdown_loop()` is called.
     /// @warning this must be called from the main thread.
@@ -30,9 +47,8 @@ public:
     /// Signals for renderer to shut down.
     void shutdown_loop();
 
-    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     // Render object lifetime.
-    //
 
     /// Creates render object.
     pool_key_t create_render_obj(Render_obj_create_config&& config);
@@ -40,9 +56,8 @@ public:
     /// Destroys render object.
     void destroy_render_obj(pool_key_t key);
 
-    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////
     // Animator controls.
-    //
 
     // @TODO.
 
