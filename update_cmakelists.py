fcd6c10de77c7f9d84f6717db05dba505f86b00b
TheaGal <timoguy99004@gmail.com>
2026-02-08 21:27:15 -0800

    updates update_cmakelists script to detect resource files as well

--- update_cmakelists.py
+++ update_cmakelists.py
@@ -1,21 +1,13 @@
 from pathlib import Path
 
 CMAKELISTS_FNAME = './CMakeLists.txt'
-START_BLOCK = 'set(MAIN_SOURCES'
-END_BLOCK = ')'
 
 
-def find_existing_files() -> list[Path]:
-    SEARCH_DIRS = ['./include/', './src/']
-    SEARCH_EXTENSIONS = ['h',
-                         'hpp',
-                         'ixx',  # I think this is for modules???
-                         'c',
-                         'cxx',
-                         'cpp']
+def find_existing_files(search_dirs: list[str], search_extensions: list[str]) -> list[Path]:
+    
     all_found_files: list[Path] = []
-    for search_dir in SEARCH_DIRS:
-        for search_ext in SEARCH_EXTENSIONS:
+    for search_dir in search_dirs:
+        for search_ext in search_extensions:
             # Convert `search_ext` to case-insensitive extension.
             case_insensitive_ext = '*.'
             for ext_char in search_ext:
@@ -32,23 +24,22 @@ def prepend_src_file_entry(fname: Path) -> str:
     return '    ${CMAKE_CURRENT_SOURCE_DIR}/' + f'{fname.as_posix()}'
 
 
-def read_existing_surrounding_src_entries_strings() -> tuple[str, str]:
+def read_existing_surrounding_src_entries_strings(start_block: str, end_block: str) -> tuple[str, str]:
     before_str_block = ''
     after_str_block = ''
 
     block_process = 0  # 0:before; 1:within; 2:after;
-    found_source_files = []
     with open(CMAKELISTS_FNAME, 'r') as f:
         for line in f:
             if block_process == 0:
                 # Add before str line.
                 before_str_block += line
 
-                if line.strip() == START_BLOCK:
+                if line.strip() == start_block:
                     # Found start of block.
                     block_process = 1
             elif block_process == 1:
-                if line.strip() == END_BLOCK:
+                if line.strip() == end_block:
                     # Add after str line.
                     after_str_block += line
 
@@ -61,15 +52,43 @@ def read_existing_surrounding_src_entries_strings() -> tuple[str, str]:
     return before_str_block, after_str_block
 
 
-if __name__ == '__main__':
-    existing_files = find_existing_files()
+def update_file_entries(existing_file_search_dirs: list[str],
+                        search_extensions: list[str],
+                        cmakelists_start_block: str,
+                        cmakelists_end_block: str):
+    existing_files = find_existing_files(existing_file_search_dirs, search_extensions)
     src_entries = [prepend_src_file_entry(x) for x in existing_files]
     src_entries.sort()
     src_entries_str_block = '\n'.join(src_entries) + '\n'  # Add newline at end of block.
 
-    before_str_block, after_str_block = read_existing_surrounding_src_entries_strings()
+    before_str_block, after_str_block = \
+        read_existing_surrounding_src_entries_strings(cmakelists_start_block,
+                                                      cmakelists_end_block)
 
     complete_str_data = before_str_block + src_entries_str_block + after_str_block
     
     with open(CMAKELISTS_FNAME, 'w') as f:
         f.write(complete_str_data)
+
+
+if __name__ == '__main__':
+    # C++ source files.
+    CPP_SEARCH_EXTENSIONS = ['h',
+                             'hpp',
+                             'ixx',  # I think this is for modules???
+                             'c',
+                             'cxx',
+                             'cpp']
+    update_file_entries(['./include/', './src/'],
+                        CPP_SEARCH_EXTENSIONS,
+                        'set(MAIN_SOURCES',
+                        ')')
+    # Resource files.
+    RES_SEARCH_EXTENSIONS = ['ktx2',
+                             'glb',
+                             'gltf',
+                             'vkshader']
+    update_file_entries(['./assets/',],
+                        RES_SEARCH_EXTENSIONS,
+                        'set(ASSET_DIR_FILES',
+                        ')')
