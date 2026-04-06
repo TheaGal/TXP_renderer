54c45e44807691a13d3676e5d5e3670d440eb404
TheaGal <timoguy99004@gmail.com>
2026-04-06 01:36:29 -0700

    wip: move rend obj processing to own func

--- Hawsoo_devnotes.md
+++ Hawsoo_devnotes.md
@@ -550,6 +550,10 @@ slangc -lang slang -profile glsl_460 -target glsl assets_raw/shaders/gradient.sl
     - [x] use the static model for per-instance placement.
     - [ ] Allocate space for vertices of the deformed mesh.
         - Maybe for now just do a copy?
+        - Aaaaa do i wanna share the same buffer for the deformed meshes and static ones?
+            - Nope nop enop. it'll be a lot of copying just for this.
+            - combining the deformed meshes might work tho? mmmm it would mean that there would be stuttering for when deformed models are added and removed.
+                - eh ig it would just be best if that just happened since making 2 buffers would suck.
 
 - [ ] Write compute shader for the animation info.
 
--- src/renderer/renderer.cpp
+++ src/renderer/renderer.cpp
@@ -121,6 +121,156 @@ struct Renderer::Impl
 
     /// Debug stats' performance times.
     std::unordered_map<Performance_time_type, float_t> perf_time_map;
+
+
+    // ---- Functions ------------------------------------------------------------------------------
+
+    void process_render_object_changes()
+    {
+        // @TODO: process this once per tick (e.g. 1/60th second) (i.e. at the same rate as the
+        //        simulation loop)
+
+        auto& m{ *this };  // @HACK
+
+        bool appended_render_object{ false };
+        bool removed_render_object{ false };
+
+        // Mark all as stale.
+        for (auto& rend_obj : m.render_object_list)
+        {
+            rend_obj.is_stale = true;
+        }
+
+        // Add new render objects and mark existing render objects as non-stale.
+        auto rend_obj_cfg_view = m.ecs_registry.view<TXP::component::Render_object_config>();
+        for (auto ecs_entity : rend_obj_cfg_view)
+        {
+            auto& rend_obj_cfg =
+                rend_obj_cfg_view.get<component::Render_object_config>(ecs_entity);
+            auto& rend_owned_data = rend_obj_cfg.renderer_owned_data;
+
+            if (rend_owned_data.pool_key == k_pool_key_process_flag)
+            {   // Need to create new render object.
+                rend_owned_data.pool_key = m.render_object_list.size();
+
+                uint16_t new_render_model_idx{ (uint16_t)-1 };
+                if (rend_obj_cfg.is_deformed)
+                {   // Create new deformed model.
+                    auto static_model_data_set_idx =
+                        m.render_model_data_collection.get_static_model_data_set_idx(
+                            rend_obj_cfg.model_name);
+
+                    new_render_model_idx = m.render_model_data_collection
+                                               .create_deformed_model_from_static_model_data_set(
+                                                   static_model_data_set_idx);
+
+                    // Add animator.
+                    bool has_root_motion_tag{
+                        m.ecs_registry.any_of<component::Animator_root_motion>(ecs_entity)
+                    };
+
+                    auto& skeletal_animator{
+                        m.ecs_registry.emplace<component_internal::Model_animator>(
+                            ecs_entity,
+                            m.render_model_data_collection.get_deformed_model_anim_set(
+                                m.render_model_data_collection.get_deformed_model_anim_set_idx(
+                                    rend_obj_cfg.model_name)),
+                            m.render_model_data_collection.get_deformed_model_skin(
+                                m.render_model_data_collection.get_deformed_model_skin_idx(
+                                    rend_obj_cfg.model_name)),
+                            has_root_motion_tag)
+                    };
+
+                    BT::service_finder::find_service<Animator_template_bank>()
+                        .load_animator_template_into_animator(skeletal_animator,
+                                                            rend_obj_cfg.model_name);
+
+                    // Set first anim as default state-set.
+                    assert(!skeletal_animator.get_animator_states().empty());
+                    skeletal_animator.change_state_set(
+                        { .anim_state_indices = { 0 }, .loop_final_state = true });
+
+                    // Add anim frame action controller.
+                    if (anim_frame_action::Bank::has(rend_obj_cfg.model_name))
+                    {
+                        auto& afa_ctrller_ref{ anim_frame_action::Bank::get(
+                            rend_obj_cfg.model_name) };
+
+                        skeletal_animator.configure_anim_frame_action_controls(
+                            &afa_ctrller_ref,
+                            m.ecs_registry.get<component::Entity_metadata>(ecs_entity).uuid,
+                            component_internal::Model_animator::
+                                make_jump_queue_create_list_from_anim_frame_action_controls(
+                                    afa_ctrller_ref));
+
+                        // Add hitcapsule set driver.
+                        m.ecs_registry
+                            .emplace_or_replace<component::Animator_driven_hitcapsule_set>(
+                                ecs_entity);
+                    }
+                }
+                else
+                {
+                    new_render_model_idx =
+                        m.render_model_data_collection.get_static_model_data_set_idx(
+                            rend_obj_cfg.model_name);
+                }
+                m.render_model_data_collection.report_one_user_added(new_render_model_idx);
+
+                m.render_object_list.emplace_back(Render_object{
+                    .layer = rend_obj_cfg.render_layer,
+                    .render_model_idx = new_render_model_idx,
+                    .material_palette_idx = m.material_organizer.get_material_palette_idx(
+                        !rend_obj_cfg.material_palette.empty()
+                            ? rend_obj_cfg.material_palette
+                            : rend_obj_cfg.model_name + "__default_material_palette_name__"),
+                });
+            }
+            else
+            {   // Mark as non-stale.
+                m.render_object_list[rend_owned_data.pool_key].is_stale = false;
+            }
+
+            // Update transform.
+            // @TODO: adding interpolation here (i.e. just copying both the a->b transforms).
+            glm_mat4_copy(rend_obj_cfg.transform.raw,
+                        m.render_object_list[rend_owned_data.pool_key].transform);
+        }
+
+        // Delete stale render objects.
+        std::unordered_map<int32_t, int32_t> old_to_new_idx_map;
+        old_to_new_idx_map.reserve(rend_obj_cfg_view.size());
+
+        for (auto i = static_cast<int32_t>(m.render_object_list.size()) - 1; i >= 0; i--)
+        {
+            if (m.render_object_list[i].is_stale)
+            {
+                m.render_model_data_collection.report_one_user_removed(
+                    m.render_object_list[i].render_model_idx);
+
+                m.render_object_list.erase(m.render_object_list.begin() + i);
+                removed_render_object = true;
+            }
+            else
+            {
+                old_to_new_idx_map.emplace(i, old_to_new_idx_map.size());
+            }
+        }
+        for (auto& [_, new_idx] : old_to_new_idx_map)
+        {   // Flip since last loop was iterating backwards.
+            new_idx = (old_to_new_idx_map.size() - 1 - new_idx);
+        }
+
+        // Apply new pool keys.
+        for (auto ecs_entity : rend_obj_cfg_view)
+        {
+            auto& rend_obj_cfg =
+                rend_obj_cfg_view.get<TXP::component::Render_object_config>(ecs_entity);
+
+            rend_obj_cfg.renderer_owned_data.pool_key =
+                old_to_new_idx_map.at(rend_obj_cfg.renderer_owned_data.pool_key);
+        }
+    }
 };
 
 
@@ -256,142 +406,8 @@ void Renderer::render_one_frame(float_t delta_time)
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     // Process render object changes.
-    // @TODO: put this into its own func.
-    // @TODO: process this once per tick (e.g. 1/60th second) (i.e. at the same rate as the
-    //        simulation loop)
-
-    bool appended_render_object{ false };
-    bool removed_render_object{ false };
-
-    // Mark all as stale.
-    for (auto& rend_obj : m.render_object_list)
-    {
-        rend_obj.is_stale = true;
-    }
-
-    // Add new render objects and mark existing render objects as non-stale.
-    auto rend_obj_cfg_view = m.ecs_registry.view<TXP::component::Render_object_config>();
-    for (auto ecs_entity : rend_obj_cfg_view)
-    {
-        auto& rend_obj_cfg =
-            rend_obj_cfg_view.get<component::Render_object_config>(ecs_entity);
-        auto& rend_owned_data = rend_obj_cfg.renderer_owned_data;
-
-        if (rend_owned_data.pool_key == k_pool_key_process_flag)
-        {   // Need to create new render object.
-            rend_owned_data.pool_key = m.render_object_list.size();
-
-            uint16_t new_render_model_idx{ (uint16_t)-1 };
-            if (rend_obj_cfg.is_deformed)
-            {   // Create new deformed model.
-                auto static_model_data_set_idx =
-                    m.render_model_data_collection.get_static_model_data_set_idx(
-                        rend_obj_cfg.model_name);
-
-                new_render_model_idx =
-                    m.render_model_data_collection.create_deformed_model_from_static_model_data_set(
-                        static_model_data_set_idx);
-
-                // Add animator.
-                bool has_root_motion_tag{ m.ecs_registry.any_of<component::Animator_root_motion>(
-                    ecs_entity) };
-
-                auto& skeletal_animator{ m.ecs_registry.emplace<component_internal::Model_animator>(
-                    ecs_entity,
-                    m.render_model_data_collection.get_deformed_model_anim_set(
-                        m.render_model_data_collection.get_deformed_model_anim_set_idx(
-                            rend_obj_cfg.model_name)),
-                    m.render_model_data_collection.get_deformed_model_skin(
-                        m.render_model_data_collection.get_deformed_model_skin_idx(
-                            rend_obj_cfg.model_name)),
-                    has_root_motion_tag) };
-
-                BT::service_finder::find_service<Animator_template_bank>()
-                    .load_animator_template_into_animator(skeletal_animator,
-                                                          rend_obj_cfg.model_name);
-
-                // Set first anim as default state-set.
-                assert(!skeletal_animator.get_animator_states().empty());
-                skeletal_animator.change_state_set(
-                    { .anim_state_indices = { 0 }, .loop_final_state = true });
-
-                // Add anim frame action controller.
-                if (anim_frame_action::Bank::has(rend_obj_cfg.model_name))
-                {
-                    auto& afa_ctrller_ref{ anim_frame_action::Bank::get(rend_obj_cfg.model_name) };
-
-                    skeletal_animator.configure_anim_frame_action_controls(
-                        &afa_ctrller_ref,
-                        m.ecs_registry.get<component::Entity_metadata>(ecs_entity).uuid,
-                        component_internal::Model_animator::
-                            make_jump_queue_create_list_from_anim_frame_action_controls(
-                                afa_ctrller_ref));
-
-                    // Add hitcapsule set driver.
-                    m.ecs_registry.emplace_or_replace<component::Animator_driven_hitcapsule_set>(
-                        ecs_entity);
-                }
-            }
-            else
-            {
-                new_render_model_idx = m.render_model_data_collection.get_static_model_data_set_idx(
-                    rend_obj_cfg.model_name);
-            }
-            m.render_model_data_collection.report_one_user_added(new_render_model_idx);
-
-            m.render_object_list.emplace_back(Render_object{
-                .layer = rend_obj_cfg.render_layer,
-                .render_model_idx = new_render_model_idx,
-                .material_palette_idx = m.material_organizer.get_material_palette_idx(
-                    !rend_obj_cfg.material_palette.empty()
-                        ? rend_obj_cfg.material_palette
-                        : rend_obj_cfg.model_name + "__default_material_palette_name__"),
-            });
-        }
-        else
-        {   // Mark as non-stale.
-            m.render_object_list[rend_owned_data.pool_key].is_stale = false;
-        }
-
-        // Update transform.
-        // @TODO: adding interpolation here (i.e. just copying both the a->b transforms).
-        glm_mat4_copy(rend_obj_cfg.transform.raw,
-                      m.render_object_list[rend_owned_data.pool_key].transform);
-    }
-
-    // Delete stale render objects.
-    std::unordered_map<int32_t, int32_t> old_to_new_idx_map;
-    old_to_new_idx_map.reserve(rend_obj_cfg_view.size());
-
-    for (auto i = static_cast<int32_t>(m.render_object_list.size()) - 1; i >= 0; i--)
-    {
-        if (m.render_object_list[i].is_stale)
-        {
-            m.render_model_data_collection.report_one_user_removed(
-                m.render_object_list[i].render_model_idx);
-
-            m.render_object_list.erase(m.render_object_list.begin() + i);
-            removed_render_object = true;
-        }
-        else
-        {
-            old_to_new_idx_map.emplace(i, old_to_new_idx_map.size());
-        }
-    }
-    for (auto& [_, new_idx] : old_to_new_idx_map)
-    {   // Flip since last loop was iterating backwards.
-        new_idx = (old_to_new_idx_map.size() - 1 - new_idx);
-    }
-
-    // Apply new pool keys.
-    for (auto ecs_entity : rend_obj_cfg_view)
-    {
-        auto& rend_obj_cfg =
-            rend_obj_cfg_view.get<TXP::component::Render_object_config>(ecs_entity);
-
-        rend_obj_cfg.renderer_owned_data.pool_key =
-            old_to_new_idx_map.at(rend_obj_cfg.renderer_owned_data.pool_key);
-    }
+    m.process_render_object_changes();
+    
 
     ////////////////////////////////////////////////////////////////////////////////////////////
     // Update renderer-timed things.
