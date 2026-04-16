de416166edc239c3471e3d2ff5218b6b7f27baa4
TheaGal <timoguy99004@gmail.com>
2026-04-15 18:55:26 -0700

    notes

--- include/txp_renderer/animator/skeletal_animator.h
+++ include/txp_renderer/animator/skeletal_animator.h
@@ -14,7 +14,7 @@ private:
     void* m_animator;
 
     /// Private ctor.
-    Skeletal_animator(void* internal_animator);
+    Skeletal_animator(void* internal_animator);  // @TODO: add reference counting for this!!! (pay attention to move ctors and dtors, or delete move/copy ctors).
 
     friend class Renderer;
 };
